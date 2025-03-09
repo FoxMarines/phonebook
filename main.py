83822e36dc568c1dbda42bf643fd060e605033ad
FoxMarines <foxuamarines@gmail.com>
2025-03-09 16:40:57 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+#ТЕЛЕФОННАЯ СУКА КНИГА ЕБИ ЕЕ МАТЬ БЛЯТЬ!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+phones = [{"f_name": "Oleh", "l_name": "Usov", "phone": "736340103"}, {"f_name": "Вова", "l_name": "Свиня", "phone": "103"}]
+
+def search_phone(search_dict: str):
+   result = []
+
+   for phone in phones:
+            if search_dict in phone:
+             result.append(phone)
+   return result
+
+def add_phone(f_name, l_name,phone: str):
+    result = {"f_name": f_name, "l_name": l_name, "phone": phone}
+    print(result)
+    phones.append(result)
+
+if __name__ == "__main__":
+    result = search_phone("Ol")
+    print(result)
+    result = search_phone("oleh")
+    print(result)
+    add_phone("Vova", "Fox", "0928321212")
+print(phones)
+
+def delete_phone(name: str):
+    for phone in phones:
+     if phone["f_name"] or ["l_name"] or ["phone"] == name:
+      phones.remove(phone)
+
+delete_phone("Oleh")
+print(phones)
+
