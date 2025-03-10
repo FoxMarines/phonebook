621853e64cffbc7767c774640ee92ed6fbd371e5
FoxMarines <foxuamarines@gmail.com>
2025-03-10 14:44:08 +0200

    Update main.py
    fixed

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-#ТЕЛЕФОННАЯ СУКА КНИГА ЕБИ ЕЕ МАТЬ БЛЯТЬ!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# ТЕЛЕФОННАЯ СУКА КНИГА ЕБИ ЕЕ МАТЬ БЛЯТЬ!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 phones = []
 
-def search_phone(search_contact: str):
-   result = []
-   for phone in phones:
-    if search_contact in phone:
-     result.append(phone)
-   return result
 
-def add_phone(f_name: str, l_name: str,phone: str):
+def search_phone(search_contact: str):
+    result = []
+    for phone in phones:
+        if search_contact in phone:
+            result.append(phone)
+    return result
+
+
+def add_phone(f_name: str, l_name: str, phone: str):
     result = {"f_name": f_name, "l_name": l_name, "phone": phone}
     phones.append(result)
 
-def delete_phone(name: str):
-   for phone in phones:
-    if phone["f_name"] or ["l_name"] or ["phone"] == name:
-     phones.remove(phone)
 
-print(phones)
+def delete_phone(name: str):
+    for phone in phones:
+        if phone["f_name"] or ["l_name"] or ["phone"] == name:
+            phones.remove(phone)
