ff6e8cea9013aeb7741cce2d0827efa7561f9fc4
FoxMarines <foxuamarines@gmail.com>
2025-03-11 12:21:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,24 @@
 #ТЕЛЕФОННАЯ СУКА КНИГА ЕБИ ЕЕ МАТЬ БЛЯТЬ!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-phones = [{"f_name": "Oleh", "l_name": "Usov", "phone": "736340103"}, {"f_name": "Вова", "l_name": "Бог", "phone": "103"}]
+phones = []
 
-def search_phone(search_dict: str):
-   result = []
 
-   for phone in phones:
-            if search_dict in phone:
-             result.append(phone)
-   return result
+def search_phone(search_contact: dict):
+    result = []
+    for phone in phones:
+        # есть ли то шо ищем в словаре с игнор. регистра .lower, !=-1 - возвращает -1 если ниче не нашло
+        if any(str(value).lower().find(str(search_contact[key]).lower()) != -1
+               for key, value in phone.items() if key in search_contact):
+            result.append(phone)
+    return result
 
-def add_phone(f_name, l_name,phone: str):
+
+def add_phone(f_name: str, l_name: str, phone: str):
     result = {"f_name": f_name, "l_name": l_name, "phone": phone}
-    print(result)
     phones.append(result)
 
-def delete_phone(name: str):
-    for phone in phones:
-     if phone["f_name"] or ["l_name"] or ["phone"] == name:
-      phones.remove(phone)
 
-delete_phone("Oleh")
-print(phones)
-
-if __name__ == "__main__":
-    result = search_phone("Ol")
-    print(result)
-    result = search_phone("oleh")
-    print(result)
-    add_phone("Vova", "Fox", "0928321212")
-print(phones)
-
+def delete_phone(f_name: str, l_name: str, phone: str):
+    for p in phones[:]:
+        if p["f_name"] == f_name and p["l_name"] == l_name and p["phone"] == phone:
+            phones.remove(p)
