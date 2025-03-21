07a251de8ede31c62deb56f7f0d07a7186428df8
FoxMarines <foxuamarines@gmail.com>
2025-03-21 15:46:09 +0200

    Update main.py
    fixed search_phone

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-#ТЕЛЕФОННАЯ СУКА КНИГА ЕБИ ЕЕ МАТЬ БЛЯТЬ!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# ТЕЛЕФОННАЯ СУКА КНИГА ЕБИ ЕЕ МАТЬ БЛЯТЬ!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 phones = []
 
 
-def search_phone(search_contact: dict):
+def search_phone(search_contact: str):
     result = []
-    for phone in phones:
-        # есть ли то шо ищем в словаре с игнор. регистра .lower, !=-1 - возвращает -1 если ниче не нашло
-        if any(str(value).lower().find(str(search_contact[key]).lower()) != -1
-               for key, value in phone.items() if key in search_contact):
-            result.append(phone)
+    for phone in phones:  # phone = dict
+        for key, value in phone.items():  # key = f_name, l_name, phone; value = vova, gospod, 1488 etc
+            if str(search_contact) in value:
+                result.append(phone)
     return result
 
 
@@ -22,3 +21,9 @@ def delete_phone(f_name: str, l_name: str, phone: str):
     for p in phones[:]:
         if p["f_name"] == f_name and p["l_name"] == l_name and p["phone"] == phone:
             phones.remove(p)
+
+
+add_phone("Vova", "Gospod", "1488")
+add_phone("Oleh", "usov", "1337")
+
+b = search_phone("")
